51b355de9ef436852306defe7e53897ee4f3472f
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2026-01-10 19:08:12 +0200

    feat: implement get all products hook

--- src/modules/products/index.tsx
+++ src/modules/products/index.tsx
@@ -1,23 +1,22 @@
-import { createContext, useContext, type PropsWithChildren } from "react"
+import { createContext, useContext, type PropsWithChildren } from "react";
+import type { ProductsRepository } from "./repository/ProductsRepository";
 
+const ProductContext = createContext<ProductsRepository | null>(null);
 
-
-const ProductContext= createContext<string | null>(null);
-
-type ProductProviderProps=PropsWithChildren<{
-    value:string | null;
+type ProductProviderProps = PropsWithChildren<{
+  value: ProductsRepository | null;
 }>;
 
-export const ProductProviders=({value , children}: ProductProviderProps)=>{
-    return(<ProductContext.Provider value={value}>{children}</ProductContext.Provider>);
-}
+export const ProductProviders = ({ value, children }: ProductProviderProps) => {
+  return (
+    <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+  );
+};
 
-
-export const useProducts=()=>{
-    const context = useContext(ProductContext);
-    if(context === null){
-        throw new Error("useProducts must be used within a ProductProvider");
-    }   
-    return context;   
-
-}
+export const useProducts = () => {
+  const context = useContext(ProductContext);
+  if (context === null) {
+    throw new Error("useProducts must be used within a ProductProvider");
+  }
+  return context;
+};
