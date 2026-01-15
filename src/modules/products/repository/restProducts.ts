57879c935b445b39fc8fb24f4eefdc115556a722
dev-mohammed-hamada <mohammedhamada.dev@gmail.com>
2026-01-15 22:05:14 +0200

    feat: (products pagination) add products pagination parameters to the functions

--- src/modules/products/repository/restProducts.ts
+++ src/modules/products/repository/restProducts.ts
@@ -1,17 +1,34 @@
-import { toProduct } from "../adapters/toProduct";
-import type { Product } from "../entities/Product";
-import type { ProductsRepository } from "./ProductsRepository";
+import { toProduct } from '../adapters/toProduct';
+import type { ProductsRepository, ProductsResponse } from '../types/repository';
 
 const Base_URL = "https://dummyjson.com/products";
 
 export const restProducts = (): ProductsRepository => {
   return {
-    getAll: async (): Promise<Product[]> => {
-      const response = await fetch(Base_URL);
+    getAll: async (params): Promise<ProductsResponse> => {
+      const queryParams = new URLSearchParams();
+
+      if (params.limit !== undefined)
+        queryParams.append('limit', params.limit.toString());
+      if (params.skip !== undefined)
+        queryParams.append('skip', params.skip.toString());
+
+      const response = await fetch(Base_URL + '?' + queryParams.toString());
       if (!response.ok) {
         throw new Error("Failed to fetch products");
       }
-      return response.json().then((data) => toProduct(data.products));
+
+      const data = await response.json();
+      const totalProducts = data.total;
+      const totalPages = Math.ceil(
+        totalProducts / (params.limit || totalProducts)
+      );
+
+      return {
+        products: toProduct(data.products),
+        totalProducts,
+        totalPages,
+      };
     },
   };
 };
