4e5a0391a5408e7cc019e91c8a5b5558eb3e4f39
mAbadsa <moh22389@gmail.com>
2026-01-15 15:52:44 +0200

    Create product container

---
+++ src/modules/products/views/ProductsContainer.tsx
@@ -0,0 +1,15 @@
+import { Title, Container } from "@mantine/core";
+import type { Product } from "../entities/Product";
+import ProductsCarousel from "./ProductsCarousel";
+
+const ProductsContainer: React.FC<{ category: string, products: Product[] }> = ({ category, products }) => {
+
+  return (
+      <Container bg={"#efd3ffff"} py={"lg"} display={"block"}>
+        <Title ta={"left"}>{category.toUpperCase()}</Title>
+        <ProductsCarousel products={products} />
+      </Container>
+  );
+};
+
+export default ProductsContainer;
