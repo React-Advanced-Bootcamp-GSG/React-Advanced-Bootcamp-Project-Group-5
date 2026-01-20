95e543882fab23075a688f57a9f38642d248340e
Nada.Salha <128420328+NadaSalha@users.noreply.github.com>
2026-01-20 21:34:21 +0200

    chore(products): integrate TanStack Router and configure base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Products from "./modules/products/views";
+import {  createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routes";
 
+const router = createRouter({ routeTree, notFoundMode: "root" })
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 function App() {
-  return (
-    <>
-      <Products />
-    </>
-  );
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
